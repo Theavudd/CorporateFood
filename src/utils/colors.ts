a9eeea81a991cdbe90c57058ce481796aa9f0c36
Krishna1001k <krishna.kumar@appinventiv.com>
2023-03-29 18:42:35 +0530

    colorFileAdded

---
+++ src/utils/colors.ts
@@ -0,0 +1,18 @@
+export const colors = {
+  orange: '#FE724C',
+  orangeLight: '#FE8160',
+  orangeLight1: '#FEA58D',
+  orangeLight2: '#FED2C7',
+  yellow: '#FFC529',
+  yellowLight: '#FFD050',
+  yellowLight1: '#FFDF8B',
+  yellowLight2: '#FFEFC3',
+  blackDark: '#1A1D26',
+  blackLight: '#2A2F3D',
+  blackLight1: '#4D5364',
+  blackLight2: '#6E7489',
+  gray: '#9A9FAE',
+  grayLight: '#A8ACB9',
+  grayLight1: '#C4C7D0',
+  grayLight2: '#EBEBEB',
+};
