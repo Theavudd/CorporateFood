7d318fa7a8b53e4409c63b3aae462ef103b6194c
ikamalk6 <kamal.kaushal@appinventiv.com>
2023-03-31 15:49:45 +0530

    splashDone

--- src/modules/splash/index.tsx
+++ src/modules/splash/index.tsx
@@ -1,13 +1,63 @@
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import React from 'react';
-import Fonts from '../../utils/fonts';
+import {colors} from '../../utils/colors';
+import {vh, vw} from '../../utils/dimensions';
+import Lottie from 'lottie-react-native';
+import fonts from '../../utils/fonts';
+import image from '../../utils/image';
 
 export default function SplashScreen() {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <Text style={{fontFamily: Fonts.Regular}}>SplashScreen</Text>
-    </SafeAreaView>
+    <View style={styles.mainView}>
+      {/* <Animated.View
+        style={{
+          transform: [
+            {
+              scale: animate,
+            },
+          ],
+
+        }}>
+        <Image source={image.splash} style={styles.floating} />
+       
+      </Animated.View> */}
+
+      <Lottie
+        loop={false}
+        autoPlay
+        speed={0.8}
+        renderMode={'AUTOMATIC'}
+        resizeMode={'contain'}
+        onAnimationFinish={() => {
+          console.log('finished');
+        }}
+        source={require('../../assets/images/splash.json')}
+      />
+      {/* <Text style={styles.heading}>{'Corporate Food'}</Text> */}
+    </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  // floating: {
+  //   height: vh(200),
+  //   width: vw(200),
+  //   alignSelf: 'center',
+  //   resizeMode: 'contain',
+  //   borderRadius: vh(400),
+  //   backgroundColor: 'white',
+  // },
+  mainView: {
+    flex: 1,
+    backgroundColor: colors.orange,
+    justifyContent: 'center',
+  },
+  heading: {
+    fontSize: vw(24),
+    fontFamily: fonts.SemiBold,
+    textAlign: 'center',
+    paddingHorizontal: vw(10),
+    paddingVertical: vh(10),
+    color: colors.white,
+  },
+});
--- src/utils/image.ts
+++ src/utils/image.ts
@@ -1,3 +1,4 @@
 export default {
   password: require('../assets/images/password.png'),
-};
+  // splash: require('../../assets/images/splash.json'),
+}
