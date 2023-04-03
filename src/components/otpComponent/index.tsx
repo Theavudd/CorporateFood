727a12488296ef79e0da8cf432aeae57dfbbc209
satyamkr7677 <satyam.tiwari@appinventiv.com>
2023-04-03 12:10:08 +0530

    added signUp

---
+++ src/components/otpComponent/index.tsx
@@ -0,0 +1,12 @@
+import {StyleSheet, Text, TextInput, View} from 'react-native';
+import React from 'react';
+
+export default function OTPComponent() {
+  return (
+    <View>
+      <TextInput />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
