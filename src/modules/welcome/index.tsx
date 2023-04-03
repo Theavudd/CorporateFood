ef11de643d711aebd33665d6da8cf9d594b4b8f7
ikamalk6 <kamal.kaushal@appinventiv.com>
2023-04-03 17:44:39 +0530

    welcomeScreenDone

--- src/modules/welcome/index.tsx
+++ src/modules/welcome/index.tsx
@@ -6,45 +6,60 @@ import ScreenNames from '../../router/screenNames';
 import {useNavigation} from '@react-navigation/native';
 import CustomButton from '../../components/customButton';
 import {
-  Text,
-  View,
-  StyleSheet,
-  SafeAreaView,
+  Image,
   ImageBackground,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
 } from 'react-native';
-import {vh, vw} from '../../utils/dimensions';
+import {SCREEN_HEIGHT, SCREEN_WIDTH, vh, vw} from '../../utils/dimensions';
 import string from '../../utils/string';
 export default function WelcomeScreen() {
   const navigation = useNavigation<any>();
+  const onSkipPress = () => {
+    navigation.navigate(ScreenNames.LOGIN);
+  };
 
   const loginButtons = () => {
     return (
-      <View style={styles.loginBtn}>
+      <View
+        style={{
+          position: 'absolute',
+          bottom: vh(60),
+          flex: 0.3,
+          width: '100%',
+        }}>
         <CustomButton
           onPress={() => {}}
           buttonText={string.google}
           leftImage={image.google}
           extraLeftImageContainerStyle={{height: '40%'}}
-          containerStyle={styles.gBtn}
-          textStyle={styles.gTxt}
+          containerStyle={{
+            backgroundColor: colors.white,
+            // width: '85%',
+            marginHorizontal: vw(25),
+          }}
+          textStyle={{color: colors.blackDark, fontFamily: fonts.SemiBold}}
         />
         <CustomButton
-          onPress={() => {
-            navigation.replace(ScreenNames.LOGIN, {showBackButton: false});
-          }}
+          onPress={() => {}}
           buttonText={string.loginWithEmail}
           extraLeftImageContainerStyle={{height: '40%'}}
-          containerStyle={styles.lBtn}
-          textStyle={styles.lTxt}
+          containerStyle={{
+            backgroundColor: 'rgba(255, 255, 255, 0.21)',
+            borderWidth: 1,
+            borderColor: colors.white,
+            marginVertical: vh(23),
+            marginHorizontal: vw(25),
+          }}
+          textStyle={{color: colors.white, fontFamily: fonts.Regular}}
         />
         <Text style={styles.dontHaveAccount}>
           {string.dontHaveAccount}
-          <Text
-            style={styles.signUp}
-            onPress={() => {
-              navigation.replace(ScreenNames.SIGNUP, {showBackButton: false});
-            }}>
-            {string.signUp}
+          <Text style={styles.signUp} onPress={() => {}}>
+            {string.login}
           </Text>
         </Text>
       </View>
@@ -55,10 +70,20 @@ export default function WelcomeScreen() {
       resizeMode="stretch"
       style={styles.main}
       source={image.welcome}>
-      <SafeAreaView style={styles.safeView}>
-        <View style={styles.headView}>
-          <Text style={styles.welcome}>{string.welcomeTo}</Text>
-          <Text style={styles.corporate}>{string.corporatefood}</Text>
+      <SafeAreaView style={{flex: 1, marginTop: vh(40)}}>
+        <CustomButton
+          onPress={onSkipPress}
+          buttonText="Skip"
+          containerStyle={styles.skipbtn}
+          textStyle={{color: colors.orange}}
+        />
+        <View
+          style={{
+            alignSelf: 'center',
+            marginTop: vh(50),
+          }}>
+          <Text style={styles.welcome}>Welcome to</Text>
+          <Text style={styles.corporate}>Corporate Food</Text>
         </View>
         {loginButtons()}
       </SafeAreaView>
@@ -70,10 +95,12 @@ const styles = StyleSheet.create({
   main: {
     flex: 1,
   },
-  safeView: {flex: 1, marginTop: vh(40)},
-  headView: {
-    alignSelf: 'center',
-    marginTop: vh(80),
+  skipbtn: {
+    height: vh(30),
+    width: vw(58),
+    marginHorizontal: vw(30),
+    alignSelf: 'flex-end',
+    backgroundColor: colors.white,
   },
   welcome: {
     color: colors.blackDark,
@@ -86,29 +113,10 @@ const styles = StyleSheet.create({
     fontFamily: fonts.SemiBold,
     fontSize: vw(45),
   },
-  gBtn: {
-    backgroundColor: colors.white,
-    marginHorizontal: vw(25),
-  },
-  gTxt: {color: colors.blackDark, fontFamily: fonts.SemiBold},
   socialSignInText: {
     marginHorizontal: '6%',
     fontSize: vw(10),
     color: colors.gray,
-  },
-  lBtn: {
-    backgroundColor: 'rgba(255, 255, 255, 0.21)',
-    borderWidth: 1,
-    borderColor: colors.white,
-    marginVertical: vh(23),
-    marginHorizontal: vw(25),
-  },
-  lTxt: {color: colors.white, fontFamily: fonts.Regular},
-  loginBtn: {
-    position: 'absolute',
-    bottom: vh(60),
-    flex: 0.3,
-    width: '100%',
   },
   midLine: {
     width: '35%',
